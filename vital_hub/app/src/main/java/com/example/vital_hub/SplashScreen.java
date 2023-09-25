9d92594e269d41e033322bbfdc3b5f933a8f08b9
Kwan <nhimcoi262@gmail.com>
2023-09-25 11:42:47 +0700

    Add splash screen

---
+++ vital_hub/app/src/main/java/com/example/vital_hub/SplashScreen.java
@@ -0,0 +1,24 @@
+package com.example.vital_hub;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
