139550a5533cfdee3e4ce1b63a1fa0594680a08d
anhbanlinhle <vietlinhleviet@gmail.com>
2023-09-28 18:43:03 +0700

    add googleSignInIntent (google accounts selection popup)

--- vital_hub/app/src/main/java/com/example/vital_hub/LoginScreen.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/LoginScreen.java
@@ -2,13 +2,37 @@ package com.example.vital_hub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class LoginScreen extends AppCompatActivity {
-
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 1;
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        loginBtn = findViewById(R.id.btnLogin);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent loginIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(loginIntent, RC_SIGN_IN);
+            }
+        });
     }
 }
