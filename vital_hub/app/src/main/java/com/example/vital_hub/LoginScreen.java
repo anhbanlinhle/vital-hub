bb05faabc61e9640e38e38e58efaefd9711b1579
anhensam4 <ahs2k3@gmail.com>
2023-09-28 20:44:57 +0700

    login demo 1 | button bug

---
+++ vital_hub/app/src/main/java/com/example/vital_hub/LoginScreen.java
@@ -0,0 +1,14 @@
+package com.example.vital_hub;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+    }
+}
--- vital_hub/app/src/main/java/com/example/vital_hub/MainActivity.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.vital_hub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent loginIntent = new Intent(this, LoginScreen.class);
+        startActivity(loginIntent);
     }
 }
