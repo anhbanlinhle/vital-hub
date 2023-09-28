77f5eb9f6af09a5ee045cffbb33950fc79492939
anhensam4 <ahs2k3@gmail.com>
2023-09-28 23:38:12 +0700

    implement oauth with UI | button bug

--- vital_hub/app/src/main/java/com/example/vital_hub/LoginScreen.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/LoginScreen.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -86,7 +87,7 @@ public class LoginScreen extends AppCompatActivity {
             intent.putExtra("email", email);
             intent.putExtra("displayName", displayName);
             intent.putExtra("avatar", avatar);
-
+            Log.d("adad", "login");
             startActivity(intent);
         }
     }
