fcd7a6c9150903c015e8116d0e81ca5c9aa7415d
anhbanlinhle <vietlinhleviet@gmail.com>
2023-09-28 19:32:19 +0700

    handle signed in account data from google

--- vital_hub/app/src/main/java/com/example/vital_hub/LoginScreen.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/LoginScreen.java
@@ -1,5 +1,6 @@
 package com.example.vital_hub;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,11 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 public class LoginScreen extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
@@ -34,5 +38,39 @@ public class LoginScreen extends AppCompatActivity {
                 startActivityForResult(loginIntent, RC_SIGN_IN);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            updateUI(account);
+        } catch (ApiException e) {
+
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account != null) {
+            String email = account.getEmail();
+            String displayName = account.getDisplayName();
+            String avatar = String.valueOf(account.getPhotoUrl());
+
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("email", email);
+            intent.putExtra("displayName", displayName);
+            intent.putExtra("avatar", avatar);
+
+            startActivity(intent);
+        }
     }
 }
