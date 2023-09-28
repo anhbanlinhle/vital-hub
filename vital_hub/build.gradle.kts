c7074c803ff3153918d6800cb4388c1c7c03e51c
anhbanlinhle <vietlinhleviet@gmail.com>
2023-09-28 22:23:35 +0700

    config for 1-tap auth

--- vital_hub/build.gradle.kts
+++ vital_hub/build.gradle.kts
@@ -1,4 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+
+    repositories {
+        google()
+    }
+}
+
+
+allprojects {
+    repositories {
+        google()
+    }
+}
 plugins {
     id("com.android.application") version "8.1.1" apply false
 }
