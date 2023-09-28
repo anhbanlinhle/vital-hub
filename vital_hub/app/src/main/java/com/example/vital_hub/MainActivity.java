7b731e3b407e363a8c40811e154d870641219100
anhbanlinhle <vietlinhleviet@gmail.com>
2023-09-28 19:40:31 +0700

    render account data on another activity

--- vital_hub/app/src/main/java/com/example/vital_hub/MainActivity.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.vital_hub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView email;
+    TextView displayName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        displayName = findViewById(R.id.displayName);
+
+        Intent intent = getIntent();
+        email.setText(intent.getStringExtra("email"));
+        displayName.setText(intent.getStringExtra("displayName"));
+
+
     }
 }
