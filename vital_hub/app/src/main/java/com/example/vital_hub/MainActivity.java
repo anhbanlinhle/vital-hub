3d95c3b12f460b7d50d84046b8d9f7431609ca33
anhbanlinhle <vietlinhleviet@gmail.com>
2023-09-28 19:54:31 +0700

    handle logout button

--- vital_hub/app/src/main/java/com/example/vital_hub/MainActivity.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.vital_hub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
+    private GoogleSignInClient mGoogleSignInClient;
     TextView email;
     TextView displayName;
+    Button logoutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,11 +25,24 @@ public class MainActivity extends AppCompatActivity {
 
         email = findViewById(R.id.email);
         displayName = findViewById(R.id.displayName);
+        logoutBtn = findViewById(R.id.btnLogout);
 
         Intent intent = getIntent();
         email.setText(intent.getStringExtra("email"));
         displayName.setText(intent.getStringExtra("displayName"));
 
-
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Intent intent = new Intent(MainActivity.this, LoginScreen.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+            }
+        });
     }
 }
