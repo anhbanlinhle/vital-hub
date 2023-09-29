9fc9a5bdb57c44808f3fe05085a40fa4da64421f
Kwan <nhimcoi262@gmail.com>
2023-09-29 11:58:16 +0700

    Add basic user profile

--- vital_hub/app/src/main/java/com/example/vital_hub/MainActivity.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.vital_hub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.user_profile);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUserProfile();
+            }
+        });
     }
+    public void openUserProfile(){
+        Intent intent = new Intent(this, UserProfile.class);
+        startActivity(intent);
+    }
+
 }
---
+++ vital_hub/app/src/main/java/com/example/vital_hub/UserProfile.java
@@ -0,0 +1,13 @@
+package com.example.vital_hub;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserProfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+    }
+}
