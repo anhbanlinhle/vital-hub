b930ba7a91c01e96fdb05ad29f5a024d50b0fdab
vmtuan12 <minhtuan3154@gmail.com>
2023-10-06 00:25:42 +0700

    implement fields, add validation and warning

--- vital_hub/app/src/main/java/com/example/vital_hub/authentication/FirstRegistInfo.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/authentication/FirstRegistInfo.java
@@ -2,19 +2,46 @@ package com.example.vital_hub.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.vital_hub.R;
+import com.example.vital_hub.utils.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FirstRegistInfo extends AppCompatActivity implements TextWatcher {
 
-    String[] sex = {"MALE", "FEMALE"};
+    private final String[] sex = {"MALE", "FEMALE"};
 
-    AutoCompleteTextView editSex;
+    private AutoCompleteTextView editSex;
+
+    private EditText name;
+    private EditText phone;
+    private EditText height;
+    private EditText weight;
+    private EditText exerciseDays;
+    private EditText description;
+
+    private TextView sexWarning;
+    private TextView phoneWarning;
+    private TextView nameWarning;
+
+    private TextView inadequateInfoWarning;
+
+    private List<EditText> requiredFieldsList;
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +52,116 @@ public class FirstRegistInfo extends AppCompatActivity implements TextWatcher {
     }
 
     private void firstDeclaration() {
+        name = (EditText) findViewById(R.id.edit_name);
+        phone = (EditText) findViewById(R.id.edit_phone);
         editSex = (AutoCompleteTextView) findViewById(R.id.edit_sex);
+        height = (EditText) findViewById(R.id.edit_height);
+        weight = (EditText) findViewById(R.id.edit_weight);
+        exerciseDays = (EditText) findViewById(R.id.edit_exercise_day);
+        description = (EditText) findViewById(R.id.edit_description);
+
+        sexWarning = (TextView) findViewById(R.id.sex_warning);
+        phoneWarning = (TextView) findViewById(R.id.phone_warning);
+        nameWarning = (TextView) findViewById(R.id.name_warning);
+        inadequateInfoWarning = (TextView) findViewById(R.id.warning_inadequate_info);
+
+        requiredFieldsList = new ArrayList<>();
+        requiredFieldsList.add(name);
+        requiredFieldsList.add(phone);
+        requiredFieldsList.add(editSex);
+
+        button = (Button) findViewById(R.id.btn_submit);
+
+
+        name.requestFocus();
+
+        this.editSexSetting();
+        this.requiredFieldsSetting();
+        this.buttonSetting();
+    }
+
+    private void editSexSetting() {
         editSex.addTextChangedListener(this);
         editSex.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line, sex));
+        editSex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editSex.showDropDown();
+            }
+        });
+        editSex.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (b) {
+                    editSex.showDropDown();
+                } else {
+                    if (StringUtil.isEmpty(editSex.getText())) {
+                        sexWarning.setText("Invalid sex");
+                    } else {
+                        sexWarning.setText(null);
+                        if (adequateInformation()) {
+                            inadequateInfoWarning.setText(null);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void requiredFieldsSetting() {
+        name.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b) {
+                    if (StringUtil.isEmpty(name.getText())) {
+                        nameWarning.setText("Invalid name");
+                    } else {
+                        nameWarning.setText(null);
+                        if (adequateInformation()) {
+                            inadequateInfoWarning.setText(null);
+                        }
+                    }
+                }
+            }
+        });
+
+        phone.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b) {
+                    if (StringUtil.isEmpty(phone.getText())) {
+                        phoneWarning.setText("Invalid phone number");
+                    } else {
+                        phoneWarning.setText(null);
+                        if (adequateInformation()) {
+                            inadequateInfoWarning.setText(null);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void buttonSetting() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (adequateInformation()) {
+
+                } else {
+                    inadequateInfoWarning.setText("Please fill all the fields required");
+                }
+            }
+        });
+    }
+
+    private boolean adequateInformation() {
+        for (EditText editText : requiredFieldsList) {
+            if (StringUtil.isEmpty(editText.getText())) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
@@ -42,6 +176,14 @@ public class FirstRegistInfo extends AppCompatActivity implements TextWatcher {
 
     @Override
     public void afterTextChanged(Editable editable) {
-
+        String content = editable.toString();
+        if (!Arrays.asList(sex).contains(content)) {
+            sexWarning.setText("Invalid sex");
+        } else {
+            sexWarning.setText(null);
+            if (adequateInformation()) {
+                inadequateInfoWarning.setText(null);
+            }
+        }
     }
 }
