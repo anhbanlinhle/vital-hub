9cd1b97dc91f4776efb70d2239936d21f0eaef10
vmtuan12 <minhtuan3154@gmail.com>
2023-10-05 18:00:44 +0700

    first create interface of information register

---
+++ vital_hub/app/src/main/java/com/example/vital_hub/authentication/FirstRegistInfo.java
@@ -0,0 +1,16 @@
+package com.example.vital_hub.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vital_hub.R;
+
+public class FirstRegistInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_regist_info);
+    }
+}
