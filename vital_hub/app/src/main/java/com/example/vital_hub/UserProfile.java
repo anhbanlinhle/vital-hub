4f02c9d05fbf5ad3dcf5e2216ae704d01fbc2271
Kwan <nhimcoi262@gmail.com>
2023-10-05 10:48:00 +0700

    Update user profile page

--- vital_hub/app/src/main/java/com/example/vital_hub/UserProfile.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/UserProfile.java
@@ -1,16 +1,25 @@
 package com.example.vital_hub;
 
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.PopupMenu;
 
 public class UserProfile extends AppCompatActivity {
-
+    Toolbar toolbar;
+    Button summary;
     ImageView profileImage;
 
     @Override
@@ -34,5 +43,10 @@ public class UserProfile extends AppCompatActivity {
             }
         });
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        ActionBar actionBar = getSupportActionBar();
     }
 }
