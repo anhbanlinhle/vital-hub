f4fa7a899b3dc7072ac51e3e8185d4018e9ed620
Kwan <nhimcoi262@gmail.com>
2023-10-08 10:21:06 +0700

    Small tweak and bug fix

--- vital_hub/app/src/main/java/com/example/vital_hub/SplashScreen.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/SplashScreen.java
@@ -15,7 +15,7 @@ public class SplashScreen extends Activity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                Intent intent = new Intent(SplashScreen.this, LoginScreen.class);
                 startActivity(intent);
                 finish();
             }
