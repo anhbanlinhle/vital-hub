2ec4eed92468978f9e0da6ec5434592b2303258f
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-22 23:23:41 +0700

    base structure for computer vision server

---
+++ xerver/app/models.py
@@ -0,0 +1,3 @@
+def process_video(video_data):
+    # Process the video data here
+    return processed_data
---
+++ xerver/app/routes.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+from models import process_video
+
+router = APIRouter()
+
+@router.get("/")
+async def homepage():
+    return {"message": "server ok"}
+
+@router.post("/pushup")
+async def process_video_data(video_data: bytes):
+    processed_data = process_video(video_data)
+    return {"count": processed_data}
