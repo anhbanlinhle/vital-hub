b8b6eb8307192c618324d128dd9f58f13b3cb804
vmtuan12 <minhtuan3154@gmail.com>
2023-10-23 13:22:21 +0700

    finish competition add

--- vital_hub/app/src/main/java/com/example/vital_hub/competition/AddCompeActivity.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/competition/AddCompeActivity.java
@@ -1,22 +1,30 @@
 package com.example.vital_hub.competition;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
-import android.graphics.drawable.Drawable;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.example.vital_hub.R;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.util.Locale;
 
 public class AddCompeActivity extends AppCompatActivity {
 
@@ -28,10 +36,15 @@ public class AddCompeActivity extends AppCompatActivity {
 
     private EditText startAt;
 
+    private EditText endAt;
+
     private String compeType;
 
-    private View dialogView;
-    private AlertDialog alertDialog;
+    private Button addImgBtn;
+
+    private ImageView imageView;
+
+    private final int addImgCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,18 +55,22 @@ public class AddCompeActivity extends AppCompatActivity {
     }
 
     private void initDeclaration() {
-        dialogView = View.inflate(this, R.layout.date_time_picker, null);
-        alertDialog = new AlertDialog.Builder(this).create();
 
         compeType = "";
         compeTypeView = findViewById(R.id.compe_type);
         btnRun = findViewById(R.id.btn_run);
         btnBicyle = findViewById(R.id.btn_bicycle);
         startAt = findViewById(R.id.compe_start);
+        endAt = findViewById(R.id.compe_end);
+        addImgBtn = findViewById(R.id.add_compe_img_btn);
+        imageView = findViewById(R.id.compe_img_holder);
 
-        buttonBinding();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            buttonBinding();
+        }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     private void buttonBinding() {
         btnRun.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,19 +102,55 @@ public class AddCompeActivity extends AppCompatActivity {
             }
         });
 
-        startAt.setOnClickListener(v -> {
-            DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.date_picker);
-            TimePicker timePicker = (TimePicker) dialogView.findViewById(R.id.time_picker);
-            Calendar calendar = new GregorianCalendar(datePicker.getYear(),
-                    datePicker.getMonth(),
-                    datePicker.getDayOfMonth(),
-                    timePicker.getCurrentHour(),
-                    timePicker.getCurrentMinute());
-
-            alertDialog.dismiss();
-            alertDialog.setView(dialogView);
-            alertDialog.show();
+        addImgBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_PICK_IMAGES);
+            startActivityForResult(intent, addImgCode);
         });
 
+        startAt.setOnClickListener(v -> {
+            showDateTimeDialog(startAt);
+        });
+
+        endAt.setOnClickListener(v -> {
+            showDateTimeDialog(endAt);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (data != null) {
+                Uri uri = data.getData();
+                imageView.setImageURI(uri);
+            }
+        }
+    }
+
+    private void showDateTimeDialog(EditText dateTime) {
+        Calendar calendar = Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        calendar.set(Calendar.MINUTE,minute);
+
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.US);
+
+                        dateTime.setText(simpleDateFormat.format(calendar.getTime()));
+                    }
+                };
+
+                new TimePickerDialog(AddCompeActivity.this,timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),true).show();
+            }
+        };
+        new DatePickerDialog(AddCompeActivity.this,dateSetListener,calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH)).show();
     }
 }
