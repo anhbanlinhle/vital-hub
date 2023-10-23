f14a3701b0b9a1952a9a73b7f038fa796ab84c06
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-24 02:38:24 +0700

    basic logic handler to count pushup

--- xerver/app/routes.py
+++ xerver/app/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File
 from models import process_video
 
 router = APIRouter()
@@ -8,6 +8,6 @@ async def homepage():
     return {"message": "server ok"}
 
 @router.post("/pushup")
-async def process_video_data(video_data: bytes):
+async def process_video_data(video_data: bytes = File()):
     processed_data = process_video(video_data)
     return {"count": processed_data}
