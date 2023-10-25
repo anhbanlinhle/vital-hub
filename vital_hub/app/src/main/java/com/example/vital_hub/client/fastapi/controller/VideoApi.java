d2005c62a8c2d1146be17bc5138bfc783a74bef6
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-25 23:52:42 +0700

    migrate formdata body config to init pushup call

--- vital_hub/app/src/main/java/com/example/vital_hub/client/fastapi/controller/VideoApi.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/client/fastapi/controller/VideoApi.java
@@ -1,14 +1,21 @@
 package com.example.vital_hub.client.fastapi.controller;
 
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+
 import com.example.vital_hub.client.controller.Controller;
 import com.example.vital_hub.client.fastapi.objects.PushUpResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -36,7 +43,10 @@ public class VideoApi {
         videoController = videoRetrofit.create(VideoController.class);
     }
 
-    public static void initPushupCall(MultipartBody.Part video) {
-        pushupCall = videoController.sendVideo(video);
+    public static void initPushupCall(String videoPath) {
+        File videoFile = new File(videoPath);
+        RequestBody videoRequestBody = RequestBody.create(MediaType.parse("video/*"), videoFile);
+        MultipartBody.Part videoPart = MultipartBody.Part.createFormData("video_data", videoFile.getName(), videoRequestBody);
+        pushupCall = videoController.sendVideo(videoPart);
     }
 }
