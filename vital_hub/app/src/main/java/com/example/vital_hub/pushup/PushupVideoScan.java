2ffa7fddf8e986da7d93f61129a513c8bbaf8661
anhbanlinhle <vietlinhleviet@gmail.com>
2023-10-26 00:31:31 +0700

    check & ask for read/write storage permission

--- vital_hub/app/src/main/java/com/example/vital_hub/pushup/PushupVideoScan.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/pushup/PushupVideoScan.java
@@ -5,19 +5,28 @@ import static com.example.vital_hub.client.fastapi.controller.VideoApi.initFasta
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.example.vital_hub.R;
+import com.example.vital_hub.test.TestMain;
+import com.example.vital_hub.test.TestVideo;
 
 public class PushupVideoScan extends AppCompatActivity {
-    private static final int REQUEST_CODE_SELECT_VIDEO = 1;
-    private static final int REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION = 2;
+    private static final int REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION = 0;
+    private static final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE_PERMISSION = 1;
+    private static final int REQUEST_CODE_SELECT_VIDEO = 2;
     VideoView videoView;
     Button video, back, upload;
     TextView result;
@@ -35,6 +44,22 @@ public class PushupVideoScan extends AppCompatActivity {
 
         prefs = getSharedPreferences("UserData", MODE_PRIVATE);
         initFastapi(prefs.getString("server", "10.0.2.2"));
+
+        if (ContextCompat.checkSelfPermission(
+                PushupVideoScan.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(PushupVideoScan.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_CODE_WRITE_EXTERNAL_STORAGE_PERMISSION);
+        }
+        if (ContextCompat.checkSelfPermission(
+                PushupVideoScan.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Request permission to read external storage
+            ActivityCompat.requestPermissions(PushupVideoScan.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION);
+        }
     }
 
     @Override
@@ -46,5 +71,19 @@ public class PushupVideoScan extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        if (requestCode == REQUEST_CODE_READ_EXTERNAL_STORAGE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Read permisson granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+        if (requestCode == REQUEST_CODE_WRITE_EXTERNAL_STORAGE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Write permisson granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
