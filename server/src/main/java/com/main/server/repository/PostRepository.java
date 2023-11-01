1f1b498f752531d931d6be489c48cfab929eb90f
vmtuan12 <minhtuan3154@gmail.com>
2023-11-01 23:12:09 +0700

    add image to response post

--- server/src/main/java/com/main/server/repository/PostRepository.java
+++ server/src/main/java/com/main/server/repository/PostRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query(value = "(SELECT p.user_id AS userId, p.id AS postId, u.name AS username, p.created_at AS createdAt, " +
+    @Query(value = "(SELECT p.user_id AS userId, p.id AS postId, u.name AS username, p.created_at AS createdAt, p.image AS image " +
             "p.title AS title, u.avatar AS avatar, IF(:currentUserId = p.user_id, TRUE, FALSE) AS isOwnedInt FROM post p JOIN user u ON p.user_id = u.id " +
             "WHERE p.is_deleted = FALSE AND p.user_id IN :friendList) " +
             "UNION " +
@@ -21,7 +21,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             "LIMIT :pageSize OFFSET :page", nativeQuery = true)
     List<PostDto> allPostOrderByCreatedTime(Integer page, Integer pageSize, List<Long> friendList, Long currentUserId);
 
-    @Query("SELECT p.userId AS userId, p.id AS postId, u.name AS username, p.title AS title, p.createdAt AS createdAt, u.avatar AS avatar FROM Post p JOIN User u " +
+    @Query("SELECT p.userId AS userId, p.id AS postId, u.name AS username, p.title AS title, p.createdAt AS createdAt, u.avatar AS avatar, p.image AS image FROM Post p JOIN User u " +
             "ON p.userId = u.id WHERE p.isDeleted = FALSE")
     Optional<PostDto> getPostWithUserByCid(Long id);
 
