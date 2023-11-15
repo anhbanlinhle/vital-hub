d97aedde18717a3a341b5f36bb75b76cf22d5dd2
anhbanlinhle <vietlinhleviet@gmail.com>
2023-11-16 00:29:42 +0700

    dummy layouts for testing

--- vital_hub/app/src/main/java/com/example/vital_hub/test/TestMain.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/test/TestMain.java
@@ -85,7 +85,7 @@ public class TestMain extends AppCompatActivity {
         function.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(TestMain.this, TestMap.class);
+                Intent intent = new Intent(TestMain.this, TestNav.class);
                 startActivity(intent);
             }
         });
