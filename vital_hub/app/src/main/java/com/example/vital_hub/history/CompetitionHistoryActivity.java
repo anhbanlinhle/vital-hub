8f852ec08528b8b06bb2b9edde402f4532822fa5
Kwan <nhimcoi262@gmail.com>
2023-11-26 03:33:22 +0700

    Implement Lazyload and Search in Competition History Activity

--- vital_hub/app/src/main/java/com/example/vital_hub/history/CompetitionHistoryActivity.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/history/CompetitionHistoryActivity.java
@@ -2,6 +2,8 @@ package com.example.vital_hub.history;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -15,6 +17,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.vital_hub.R;
 import com.example.vital_hub.client.spring.controller.Api;
 import com.example.vital_hub.client.spring.objects.CompetitionHistoryListResponse;
+import com.example.vital_hub.helper.KeyboardHelper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,6 +29,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class CompetitionHistoryActivity extends AppCompatActivity {
+    boolean isLoading = false;
     ImageView backButton;
     RecyclerView competitionHistoryList;
     CompetitionHistoryListAdapter competitionHistoryListAdapter;
@@ -37,12 +41,14 @@ public class CompetitionHistoryActivity extends AppCompatActivity {
     String jwt;
     Map<String, String> headers;
     Integer pageNum = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history_layout);
         initHeaderForRequest();
 
+        KeyboardHelper.setupKeyboardHiding(this);
         competitionHistories = new ArrayList<>();
         competitionHistoryList = findViewById(R.id.competition_history_list);
         backButton = findViewById(R.id.back_button);
@@ -56,6 +62,33 @@ public class CompetitionHistoryActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        searchBar.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean isFocused) {
+                if (!isFocused) {
+                    KeyboardHelper.hideKeyboard(view);
+                }
+            }
+        });
+        searchBar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String searchTerm = charSequence.toString();
+                competitionHistories.clear();
+                fetchSpecificCompetitionHistoryList(searchTerm, pageNum);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
     }
@@ -81,13 +114,64 @@ public class CompetitionHistoryActivity extends AppCompatActivity {
                     Log.d("Error", "Error: " + response.code());
                 }
             }
+
             @Override
-            public void onFailure(@NonNull Call<List<CompetitionHistoryListResponse>> call,@NonNull Throwable t) {
+            public void onFailure(@NonNull Call<List<CompetitionHistoryListResponse>> call, @NonNull Throwable t) {
+                System.out.println("Error: " + t.getMessage());
+            }
+
+        });
+
+        competitionHistoryList.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) competitionHistoryList.getLayoutManager();
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == competitionHistories.size() - 1) {
+                        isLoading = true;
+                        getMoreData();
+                    }
+                }
+            }
+        });
+    }
+    private void getMoreData() {
+        competitionHistories.add(null);
+        competitionHistories.remove(competitionHistories.size() - 1);
+        fetchCompetitionHistoryList(pageNum);
+        pageNum++;
+        isLoading = false;
+    }
+    private void fetchSpecificCompetitionHistoryList(String searchTerm, Integer pageNum) {
+        Api.initGetCompetitionHistoryList(headers, pageNum);
+        Api.getCompetitionHistoryList.clone().enqueue(new Callback<List<CompetitionHistoryListResponse>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<CompetitionHistoryListResponse>> call, @NonNull Response<List<CompetitionHistoryListResponse>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    competitionHistoryListResponse = response.body();
+                    assert competitionHistoryListResponse != null;
+                    filterCompetitionHistories(searchTerm);
+                    competitionHistoryListAdapter.notifyDataSetChanged();
+                } else {
+                    Log.d("Error", "Error: " + response.code());
+                }
+            }
+            @Override
+            public void onFailure(@NonNull Call<List<CompetitionHistoryListResponse>> call, @NonNull Throwable t) {
                 System.out.println("Error: " + t.getMessage());
             }
 
         });
     }
+    private void filterCompetitionHistories(String searchTerm) {
+        competitionHistories.clear();
 
+        for (CompetitionHistoryListResponse history : competitionHistoryListResponse) {
+            if (history.getTitle().toLowerCase().contains(searchTerm.toLowerCase())) {
+                competitionHistories.add(history);
+            }
+        }
+    }
 
 }
