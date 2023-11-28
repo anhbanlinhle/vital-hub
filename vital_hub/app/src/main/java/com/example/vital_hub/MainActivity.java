9a44652c2d5bbbe166d0f590d0c307fb62b15648
anhbanlinhle <vietlinhleviet@gmail.com>
2023-11-28 17:45:16 +0700

    change white to green

--- vital_hub/app/src/main/java/com/example/vital_hub/MainActivity.java
+++ vital_hub/app/src/main/java/com/example/vital_hub/MainActivity.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.transition.Explode;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -45,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setNavigationBarColor(ContextCompat.getColor(this, R.color.color_green));
-        window.setStatusBarColor(ContextCompat.getColor(this, R.color.color_white));
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.color_green));
 
         replaceFragment(new HomeFragment());
         bottomNavigation.show(1, true);
